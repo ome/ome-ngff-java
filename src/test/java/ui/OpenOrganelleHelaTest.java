074bb92021b39842d12f97c84622e88ee5016d70
Christian Tischer <christian.tischer@embl.de>
2022-09-15 12:26:51 +0200

    Init pom and add MultiscalesV04 class

---
+++ src/test/java/ui/OpenOrganelleHelaTest.java
@@ -0,0 +1,25 @@
+package ui;
+
+import java.io.IOException;
+
+import ome.ngff.openorganelle.OpenOrganelleS3Opener;
+
+import bdv.util.BdvFunctions;
+import mpicbg.spim.data.SpimData;
+
+public class OpenOrganelleHelaTest {
+
+    public static void main(String[] args) throws IOException {
+        showHela();
+    }
+
+    public static void showHela() throws IOException {
+        OpenOrganelleS3Opener reader = new OpenOrganelleS3Opener(
+            "https://janelia-cosem.s3.amazonaws.com",
+            "us-west-2",
+            "jrc_hela-2");
+        OpenOrganelleS3Opener.setLogging(true);
+        SpimData image = reader.readKey("jrc_hela-2.n5/em/fibsem-uint16");
+        BdvFunctions.show(image);
+    }
+}
