9771b990afbfb1c07e0472dc17c53d63abb510c6
Christian Tischer <christian.tischer@embl.de>
2022-09-15 12:27:45 +0200

    Remove unused files

--- src/test/resources/i2k-2020/open-myosin-ome-zarr.groovy
+++
@@ -1,7 +0,0 @@
-import bdv.util.BdvFunctions
-import ome.ngff.ome.zarr.openers.OMEZarrS3Opener
-
-//N5OMEZarrImageLoader.debugLogging = true;
-reader = new OMEZarrS3Opener("https://s3.embl.de", "us-west-2", "i2k-2020");
-myosin = reader.readKey("prospr-myosin.ome.zarr");
-BdvFunctions.show(myosin);
